aadda98ab489787bec7775021787d295762e3b22
Dima Bond <neverpooh@gmail.com>
2021-02-12 10:39:04 +0200

    list added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useState, useEffect} from 'react';
 import './App.css';
+import { PhonesList } from './PhonesList'
 
-function App() {
+const App = () => {
+const [phonesList, setPhonesList] = useState([]);
+
+useEffect(() => {
+  fetch('/api/phones.json')
+    .then (response => response.json())
+    .then (result => setPhonesList(result))
+}, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-2">
+          <section>
+            <p>
+              Search:
+              <input type="text" />
+            </p>
+
+            <p>
+              Sort by:
+              <select>
+                <option value="name">Alphabetical</option>
+                <option value="age">Newest</option>
+              </select>
+            </p>
+          </section>
+
+          <section>
+            <p>Shopping Cart</p>
+            <ul>
+              <li>Phone 1</li>
+              <li>Phone 2</li>
+              <li>Phone 3</li>
+            </ul>
+          </section>
+        </div>
+      </div>
+
+      <div className="col-md-10">
+        <PhonesList phonesList={phonesList}/>
+      </div>
     </div>
   );
 }
